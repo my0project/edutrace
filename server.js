f23eb456419e1bb3b361fb3fbee2f3dc89a67b21
my0project <cmukeshbgp066@gmail.com>
2026-03-22 17:33:46 +0530

    server.js

--- server.js
+++ server.js
@@ -199,5 +199,7 @@ app.post('/update-payment', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`EduTrace Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT,'0.0.0.0',() =>{ 
+    console.log(`EduTrace Server running on port:${PORT}`);
+});
